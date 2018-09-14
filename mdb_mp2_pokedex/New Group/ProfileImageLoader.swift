3f64c7957935f4c602dbb18c65bebc707d796ce5
ajaymerchia <ajaymerchia@berkeley.edu>
2018-09-14 04:11:11 -0700

    revived Kayli's Profile Code

---
+++ mdb_mp2_pokedex/New Group/ProfileImageLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileImageLoader.swift
+//  mdb_mp2_pokedex
+//
+//  Created by Kayli  Jiang on 9/14/18.
+//  Copyright © 2018 Ajay Raj Merchia. All rights reserved.
+//
+
+import Foundation
---
+++ mdb_mp2_pokedex/New Group/StatsCells.swift
@@ -0,0 +1,24 @@
+ //
+//  StatsCells.swift
+//  mdb_mp2_pokedex
+//
+//  Created by Kayli  Jiang on 9/14/18.
+//  Copyright © 2018 Ajay Raj Merchia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+ 
+ class StatsCells: UITableView {
+    var label: UILabel!
+    var view: UIImageView!
+    
+    func loadStatsTable(){
+        let PADDING: CGFloat = 20
+        let WORKING_SPACE = view.frame.width - 2 * PADDING
+        let UIComponentAbove = webSearchButton.frame.max.Y
+        
+        
+    }
+    statsTable = UITableView(frame: CGRect(x:PADDING, y:UIComponentAbove, width: WORKING_SPACE, height: view.frame.height - UIComponent
+ }
